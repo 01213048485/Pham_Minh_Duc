f8a1ea7c9c332fc1a1db3fe03f1c55064fecef0e
01213048485 <ducdz203@gmail.com>
2019-08-01 21:27:38 +0700

    .

---
+++ helloworld.py
@@ -0,0 +1,33 @@
+
+
+# year = input("enter your yob: ")
+# print(type(year))
+# print(type(2019))
+
+# "2003"
+# print(2019 - int(year))
+# interge
+# print(10 - 1)
+# print(10 x 1)
+# print(10 / 1)
+# s
+# print(type(10))
+# print(type(10))
+# print(type(10))
+# print(type(10))
+
+# print('1'+)
+
+# print('10' + '1')
+# print(10 + 1)
+
+# # lenh nhap vao
+# some_thing = input('hay trao cho anh:')
+# # lenh in ra
+# print(some_thing)
+# # hàm type để check kiểu dữ liệu
+# print(type('10'))
+# print(type(10))
+# # int() là hàm để ép kiểu từ dạng "" về dạng số còn str() là hàm chuyển từ số về ""
+# print(type(int('10')))
+
